f0c1b0e68485bff5ca92d3c7dde43799c967c2a5
Casper <133casper@gmail.com>
2019-03-22 23:08:36 +0300

    Maven modules ("Hello World")

---
+++ task2/Casper133/task2/client/src/casper/levelup/Client.java
@@ -0,0 +1,7 @@
+package casper.levelup;
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Client!");
+    }
+}
---
+++ task2/Casper133/task2/server/src/casper/levelup/Server.java
@@ -0,0 +1,7 @@
+package casper.levelup;
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Hello World from Server!");
+    }
+}
